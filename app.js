95e97e42399b6fc766e22d52c153d7faaa3fa050
Michael Nos <mnos9897@gmail.com>
2019-09-21 11:58:03 -0400

    added app file for user data

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  let addReservation = () => {
+
+    let newReserve = {
+      name : $("#customerName").val().trim(),
+      id : $("#customersID").val().trim(),
+      email : $("#customerEmail").val().trim(),
+      phone : $("#customerPhone").val().trim()
+    }
+
+  }
+
+});
